24dbede8c4d938b7a943847cb1403347dc36a3af
francismeng <francismeng@xwfintech.com>
2020-06-01 10:37:15 +0800

    feat: 登录页基本样式完成

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,44 @@
 import * as React from 'react'
 import '@/common/css/login.less'
+import { Form, Button, Checkbox, Input } from 'antd'
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+const { useState } = React
+
 function Login(){
+  const onFinish = (values: any) => {
+    console.log(values)
+  }
+  console.log(123123, process.env.NODE_ENV)
   return (
     <div className="login-wrapper">
+      <Form
+        name="normal_login"
+        className="login-form"
+        onFinish={onFinish}
+      >
+        <h3 className="form-title">系统登录</h3>
+        <Form.Item
+          name="username"
+          rules={[{ required: true, message: '请输入账号！' }]}
+        >
+          <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="账号" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: '请输入密码' }]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="账号"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            登录
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   )
 }
