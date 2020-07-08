8d025ba93d6b8ed02993bf81778e950b0f659679
francismeng <francismeng@xwfintech.com>
2020-07-08 17:50:27 +0800

    feat: 调整组件默认值

--- src/pages/components/numberAnimation.tsx
+++ src/pages/components/numberAnimation.tsx
@@ -13,14 +13,14 @@ interface NumberProps{
 }
 
 const NumberAnimation = (props: NumberProps) => {
-  const { number = 0, className = '', formatMoney = false, duration = 1000 } = props
+  const { number = 0, className = '', formatMoney = true, duration = 1000 } = props
   const initAnimation: any = null
   const [animation, setAnimation] = useState(initAnimation)
 
   useEffect(() => {
     setAnimation({
       Children: { 
-        value: typeof number === 'number' ? 10000 : 0,
+        value: typeof number === 'number' ? number : 0,
         floatLength: 2,
         formatMoney, 
       }, 
