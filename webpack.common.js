7a5763308694af963bf619612b28932455cf98ef
francismeng <francismeng@xwfintech.com>
2020-09-24 20:40:17 +0800

    feat: vendor限制为1个

--- webpack.common.js
+++ webpack.common.js
@@ -141,15 +141,15 @@ module.exports = {
       chunks: 'all',
       maxInitialRequests: Infinity,
       minSize: 0,
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name(module) {
-            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-            return `npm.${packageName.replace('@', '')}`;
-          },
-        },
-      },
+      // cacheGroups: {
+      //   vendor: {
+      //     test: /[\\/]node_modules[\\/]/,
+      //     name(module) {
+      //       const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+      //       return `npm.${packageName.replace('@', '')}`;
+      //     },
+      //   },
+      // },
     }
   }
 }
